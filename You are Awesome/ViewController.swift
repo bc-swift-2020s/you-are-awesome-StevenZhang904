5e07122f2277fdb0ad2678fced17d27a43168e5a
STEVEN ZHANG <zhangbsr@bc.edu>
2020-01-21 16:22:35 -0500

    Through 2.10

--- You are Awesome/ViewController.swift
+++ You are Awesome/ViewController.swift
@@ -11,21 +11,51 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var messageLabel: UILabel!
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var messages = ["You are awesome","You are great","You are fantanstic","Fabulous?That's you"]
+    var number=0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        print("emojil😿")
         messageLabel.text="You are fabulous."
     }
-
+    
+    @IBAction func /* changing the words shown*/
+    
+        messageButtonPressedForWords(_ sender: UIButton) {
+        messageLabel.text = messages[number]
+        number=number+1
+        if(number == messages.count){
+            number=0
+        }
+        
+    }
+    
     @IBAction func messageButtonPressedFirst(_ sender: UIButton) {
-        print("emoji😁")
         messageLabel.text="You are Awesome."
+        imageView.image=UIImage(named: "image0")
     }
     
     
     @IBAction func messageButtonPressedSecond(_ sender: UIButton) {
         messageLabel.text="You are great."
     }
+    
+    @IBAction func RandomMessgesAndPictures(_ sender: UIButton) {
+        messageLabel.text=messages[Int.random(in: 0...messages.count-1)]
+        imageView.image=UIImage(named:"image\(Int.random(in: 0...9))")
+        
+    }
+    
+    
+    
+    
 }
+
+
+
+
 
